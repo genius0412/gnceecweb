d80e7fe5c6b07d98be1b527bbfd5e821a6cadbbf
Genius0412 <genius0412.tech@gmail.com>
2024-12-31 16:14:53 -0500

    Added more assets and posts.

--- src/app/seasons/[comp]/[year]/SeasonContent.tsx
+++ src/app/seasons/[comp]/[year]/SeasonContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Markdown from 'markdown-to-jsx';
+import Image from 'next/image'
 import React from 'react'
 
 const SeasonContent = (props: any) => {
@@ -10,7 +11,15 @@ const SeasonContent = (props: any) => {
 		<div className="w-[95%] sm:w-[90%] md:w-4/5 flex flex-col justify-start items-center">
 			<h1 className="text-4xl font-black text-primary-light dark:text-primary-dark">{postMetadata.title}</h1>
 			<h2 className="text-lg text-center">{postMetadata.subtitle}</h2>
-			<Markdown className="w-full mt-12 prose prose-h1:m-0 prose-h2:m-0 dark:prose-p:text-white prose-strong:text-emerald-600 dark:prose-strong:text-amber-200 dark:prose-invert">{postContent}</Markdown>
+			<Markdown
+				options = {{
+					overrides: {
+						img: Image
+					}
+				}}
+				className="w-full mt-12 prose prose-h1:mt-5 prose-h1:mb-2 prose-h2:mb-2 prose-h3:mb-0 prose-p:my-0 dark:prose-p:text-white prose-strong:text-emerald-600 dark:prose-strong:text-amber-200 dark:prose-invert">
+				{postContent}
+			</Markdown>
 		</div>
 	)
 }
